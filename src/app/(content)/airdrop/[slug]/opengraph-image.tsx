e1b33e70130311095a7e1309e96d99d5a3441c47
Dimasell <147792802+BLACKRUN9@users.noreply.github.com>
2024-01-01 21:20:31 +0700

    dadsd

--- src/app/(content)/airdrop/[slug]/opengraph-image.tsx
+++ src/app/(content)/airdrop/[slug]/opengraph-image.tsx
@@ -1,36 +1,46 @@
-import { ImageResponse } from "next/server"
-import { urlFor } from "@/utils/sanity-utils";
-import { airdropQuery} from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/sanityFetch";
-import { SanityDocument } from "@sanity/client";
+import { ImageResponse } from 'next/server';
+import { urlFor } from '@/utils/sanity-utils';
+import { airdropQuery } from '@/sanity/lib/queries';
+import { sanityFetch } from '@/sanity/lib/sanityFetch';
+import { SanityDocument } from '@sanity/client';
+import { toPng } from 'html-to-image';
 
 export const size = {
-    width: 1300,
-    height: 727,
+  width: 1300,
+  height: 727,
 };
 
-export const contentType = "image/*";
+export const contentType = 'image/png'; // Update based on the actual content type
 
 interface Props {
-    params: {
-        slug: string;
-    };
+  params: {
+    slug: string;
+  };
 }
 
-export default async function og({ params }: Props) {
-    const airdrop = await sanityFetch<SanityDocument>({
-        query: airdropQuery,
-        params,
-    });
+export default async function og({ params }: Props): Promise<ImageResponse> {
+  const airdrop = await sanityFetch<SanityDocument>({
+    query: airdropQuery,
+    params,
+  });
 
-    return new ImageResponse(
-        (
-            <div tw='relative fixed flex items-center justify-center'>
-                <img
-                    src={`${urlFor(airdrop?.mainImage?.asset?._ref)}`}
-                    alt={airdrop?.title}
-                />
-            </div>
-        )
-    );
+  // Render the JSX element
+  const jsxElement = (
+    <div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <img
+        src={`${urlFor(airdrop?.mainImage?.asset?._ref)}`}
+        alt={airdrop?.title}
+        style={{ width: '100%', height: '100%' }}
+      />
+    </div>
+  );
+
+  // Convert the JSX element to an image
+  const imageBase64 = await toPng(jsxElement);
+
+  // Convert the base64 image to a Buffer
+  const imageBuffer = Buffer.from(imageBase64!.split(',')[1], 'base64');
+
+  // Return the ImageResponse with the Buffer
+  return new ImageResponse(imageBuffer, { headers: { 'Content-Type': contentType } });
 }
