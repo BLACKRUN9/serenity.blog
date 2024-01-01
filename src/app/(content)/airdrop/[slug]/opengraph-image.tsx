b23fedb4199e3175610261c314b12802ce9588c7
Dimasell <147792802+BLACKRUN9@users.noreply.github.com>
2024-01-01 21:24:16 +0700

    xccxb

--- src/app/(content)/airdrop/[slug]/opengraph-image.tsx
+++ src/app/(content)/airdrop/[slug]/opengraph-image.tsx
@@ -1,46 +1,36 @@
-import { ImageResponse } from 'next/server';
-import { urlFor } from '@/utils/sanity-utils';
-import { airdropQuery } from '@/sanity/lib/queries';
-import { sanityFetch } from '@/sanity/lib/sanityFetch';
-import { SanityDocument } from '@sanity/client';
-import { toPng } from 'html-to-image';
+import { ImageResponse } from "next/server"
+import { urlFor } from "@/utils/sanity-utils";
+import { airdropQuery} from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { SanityDocument } from "@sanity/client";
 
 export const size = {
-  width: 1300,
-  height: 727,
+    width: 1300,
+    height: 727,
 };
 
-export const contentType = 'image/png'; // Update based on the actual content type
+export const contentType = "image/*";
 
 interface Props {
-  params: {
-    slug: string;
-  };
+    params: {
+        slug: string;
+    };
 }
 
-export default async function og({ params }: Props): Promise<ImageResponse> {
-  const airdrop = await sanityFetch<SanityDocument>({
-    query: airdropQuery,
-    params,
-  });
+export default async function og({ params }: Props) {
+    const airdrop = await sanityFetch<SanityDocument>({
+        query: airdropQuery,
+        params,
+    });
 
-  // Render the JSX element
-  const jsxElement = (
-    <div style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <img
-        src={`${urlFor(airdrop?.mainImage?.asset?._ref)}`}
-        alt={airdrop?.title}
-        style={{ width: '100%', height: '100%' }}
-      />
-    </div>
-  );
-
-  // Convert the JSX element to an image
-  const imageBase64 = await toPng(jsxElement);
-
-  // Convert the base64 image to a Buffer
-  const imageBuffer = Buffer.from(imageBase64!.split(',')[1], 'base64');
-
-  // Return the ImageResponse with the Buffer
-  return new ImageResponse(imageBuffer, { headers: { 'Content-Type': contentType } });
+    return new ImageResponse(
+        (
+            <div tw='relative fixed flex items-center justify-center'>
+                <img
+                    src={`${urlFor(airdrop?.mainImage?.asset?._ref)}`}
+                    alt={airdrop?.title}
+                />
+            </div>
+        )
+    );
 }
